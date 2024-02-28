48c2310b8d4417a6a176cf95203041303c4a1cab
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2024-02-28 18:16:05 +0200

    HomePage

--- Client/src/components/smallBook/SmallBook.jsx
+++ Client/src/components/smallBook/SmallBook.jsx
@@ -1,53 +1,26 @@
-// SmallBook.jsx
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./SmallBook.css";
 
-function SmallBook({ Book }) {
-  const { title, pages, author, createdAt, likes } = Book || {};
-  const navigate = useNavigate();
+function SmallBook(Book) {
 
-  if (!Book) {
-    return null;
-  }
     const BookId= Book._id;
     const BookTitle= Book.title;
-
+    const FrontImg= Book.pages[0];
+    const BackImg= Book.pages[pages.length-1];
     const Author= Book.author;
+    const createdAt= Book.createdAt;
 
-  const FrontImg = pages ? pages[0] : null;
-  const BackImg = pages ? pages[pages.length - 1] : null;
-  const formattedDate = new Date(createdAt).toLocaleDateString();
-
-  const handleBookClick = () => {
-    console.log(Book)
-    navigate("/ViewBook", { state:Book });
-  };
 
   return (
-    <div className="book-container" onClick={handleBookClick}>
-      <p className="book-title">{title}</p>
-      <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
-            <img src={FrontImg} alt="Book Cover" />
-          </div>
+    <>
+     <p>{BookTitle}</p>
+     <img src= {FrontImg} alt="missing img" />
+     <div>
+        <span>{Author}</span>
+        <span>{createdAt}</span>
 
-          <div className="flip-card-back">
-            <div className="back-container">
-              <img className="backimg" src={BackImg} alt="" />
-            </div>
-          </div>
-        </div>
-      </div>
+     </div>
 
-      <div className="book-details">
-        <div className="book-metadata">
-          <span className="book-likes">Likes: {likes}</span>
-          <span className="book-date">Date: {formattedDate}</span>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
