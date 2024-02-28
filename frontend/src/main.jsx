27f330e3f091cf99fe9b740cedcb9a16bdd6f7a4
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2024-02-28 12:40:07 +0200

    Navbar in progress

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { ContextProvider } from './Context.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import './index.css';
+import { ContextProvider } from "./Context.jsx";
+import { CssBaseline } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CssBaseline/>
     <ContextProvider>
-    <App />
+      <App />
     </ContextProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
