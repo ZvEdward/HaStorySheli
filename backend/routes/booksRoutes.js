2eea2b9a919f7cc62362afd9a8b810ae9cb426d2
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2024-02-28 13:22:32 +0200

    Maximal Merge

--- backend/routes/booksRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const booksController = require("../controllers/booksController");
-const router = express.Router();
-
-router.route("/createBook").post(booksController.createBook);
-router.route("/deleteBook").delete(booksController.deleteBook);
-module.exports = router;
