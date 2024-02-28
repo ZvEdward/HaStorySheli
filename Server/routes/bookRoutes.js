a84b6f643643b44654a62443232f267dc87345e3
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2024-02-28 18:16:05 +0200

    HomePage

--- Server/routes/bookRoutes.js
+++ Server/routes/bookRoutes.js
@@ -4,5 +4,4 @@ const router = express.Router();
 
 router.route("/createBook").post(booksController.createBook);
 router.route("/deleteBook").delete(booksController.deleteBook);
-router.route("/getMostLike/:limit").get(booksController.getTopLikedBooks);
 module.exports = router;
