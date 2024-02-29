4344e84c7a0b4fc6ffba61332d2fa7b9a65c3227
ArielOhana <140969225+ArielOhana@users.noreply.github.com>
2024-02-29 08:15:05 +0200

    Almost Finale

--- Server/controllers/bookController.js
+++ Server/controllers/bookController.js
@@ -3,66 +3,60 @@ const Books = require("../models/bookModel");
 
 exports.createBook = async (req, res) => {
   try {
-    const { title, pages, userId, summary, hashtags } = req.body;
-
-    const authorExists = await Users.findOne({ _id: userId });
-
-    if (!authorExists) {
-      return res.status(404).send({ message: 'Author not found' });
+      console.log(req.body);
+      const { title, pages, userId, summary,hashtags} = req.body;
+      console.log(userId);
+      const authorExists = await Users.findOne({_id:userId});
+      console.log(authorExists);
+  
+      if (!authorExists) {
+        return res.status(404).send({ message: 'Author not found' });
+      }
+      const newBook = new Books({
+        title,
+        pages,
+        author: userId,
+        summary,
+        hashtags,
+      });
+  
+      const savedBook = await newBook.save();
+        await Users.findByIdAndUpdate(userId, {
+        $push: { myBooks: savedBook._id },
+      });
+      res.status(201).send(savedBook);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: 'Internal Server Error' });
     }
+  };
 
-    const newBook = new Books({
-      title,
-      pages,
-      author: userId,
-      summary,
-      hashtags,
-    });
+  exports.deleteBook = async (req, res) => {
+    try {
+        const { bookId, authorId } = req.body;
 
-    const savedBook = await newBook.save();
+        const bookExists = await Books.exists({ _id: bookId });
 
-    await Users.findByIdAndUpdate(userId, {
-      $push: { myBooks: savedBook._id },
-    });
+        if (!bookExists) {
+            return res.status(404).send({ error: 'Book not found' });
+        }
+        await Users.findByIdAndUpdate(authorId, {
+            $pull: { myBooks: bookId },
+        });
+        await Books.findByIdAndDelete(bookId);
 
-    res.status(201).send(savedBook);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: 'Internal Server Error' });
-  }
-};
-
-exports.deleteBook = async (req, res) => {
-  try {
-    const { bookId, authorId } = req.body;
-
-    const bookExists = await Books.exists({ _id: bookId });
-
-    if (!bookExists) {
-      return res.status(404).send({ error: 'Book not found' });
+        res.status(200).send({ message: 'Book deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: 'Internal Server Error' });
     }
-
-    await Users.findByIdAndUpdate(authorId, {
-      $pull: { myBooks: bookId },
-    });
-
-    await Books.findByIdAndDelete(bookId);
-
-    res.status(200).send({ message: 'Book deleted successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: 'Internal Server Error' });
-  }
 };
-
 exports.getTopLikedBooks = async (req, res) => {
   try {
     let limit = req.params.limit;
-    if (limit > 10) limit = 10;
-
-    const topLikedBooks = await Books.find({ approved: false })
-      .sort({ likes: -1 })
-      .limit(limit);
+    if(limit >10)
+    limit = 10;
+    const topLikedBooks = await Books.find().sort({ likes: -1 }).limit(limit);
 
     res.status(200).send(topLikedBooks);
   } catch (error) {
@@ -70,15 +64,12 @@ exports.getTopLikedBooks = async (req, res) => {
     res.status(500).send({ message: 'Internal Server Error' });
   }
 };
-
 exports.getLatestBooks = async (req, res) => {
   try {
     let limit = req.params.limit;
     if (limit > 10) limit = 10;
 
-    const latestBooks = await Books.find({ approved: false })
-      .sort({ createdAt: -1 })
-      .limit(limit);
+    const latestBooks = await Books.find().sort({ createdAt: -1 }).limit(limit);
 
     res.status(200).send(latestBooks);
   } catch (error) {
@@ -86,3 +77,4 @@ exports.getLatestBooks = async (req, res) => {
     res.status(500).send({ message: 'Internal Server Error' });
   }
 };
+
